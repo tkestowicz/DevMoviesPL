65f3c32dda2337eacb815d2c193bea582f8c6ece
tkestowicz <tkestowicz@gmail.com>
2014-11-08 17:14:51 +0100

    Implemented order by component for videos module.

--- TheDevelopersStuff/TheDevelopersStuff.Web.UI/Scripts/components/videosList.jsx
+++ TheDevelopersStuff/TheDevelopersStuff.Web.UI/Scripts/components/videosList.jsx
@@ -4,26 +4,77 @@
 
  var VideosOrder = React.createClass({
 
+ 	getInitialState: function(){
+
+ 		var currentField = this.props.orders.filter(function(field){
+ 			return field.field === this.props.currentSettings.PropertyName;
+ 		}, this);
+
+ 		return {
+ 			current: {
+ 				field: currentField[0],
+ 				direction: this.props.currentSettings.Direction
+ 			}
+ 		};
+ 	},
+
+ 	sortOrderChanged: function(event){
+
+ 		var target = event.target;
+
+ 		event.preventDefault();
+
+ 		if(event.target.nodeName === "SPAN")
+ 		{
+ 			target = event.target.parentElement;
+ 		}
+
+ 		var data = $(target).data(),
+ 			currentField = this.props.orders.filter(function(field){
+ 				return field.field === data.field;
+ 			}, this);
+
+ 		this.setState({ current: {
+ 			field: currentField[0],
+ 			direction: data.orderDirection
+ 		}});
+
+ 		$('#'+this.props.componentName)
+ 		.trigger("changeOrder", {
+ 				PropertyName: data.field,
+ 				Direction: data.orderDirection
+		});
+ 	},
+
  	render: function(){
+ 		
+ 		var icon = (this.state.current.direction === this.props.directions.ascending) 
+ 						? 'glyphicon glyphicon-sort-by-attributes'
+ 						: 'glyphicon glyphicon-sort-by-attributes-alt',
+ 			options = this.props.orders.map(function(field){
+
+ 				var active = field === this.state.current.field;
+
+	 			return (<div>
+			                <li className="divider"></li>
+				            <li className={active && this.state.current.direction == this.props.directions.ascending ? 'active' : '' }>
+				            	<a href="#" onClick={this.sortOrderChanged} data-order-direction={this.props.directions.ascending} data-field={field.field}>{field.label} <span className="glyphicon glyphicon-sort-by-attributes"></span></a>
+			            	</li>
+				            <li className={active && this.state.current.direction == this.props.directions.descending ? 'active' : '' }>
+				            	<a href="#" onClick={this.sortOrderChanged}  data-order-direction={this.props.directions.descending} data-field={field.field}>{field.label} <span className="glyphicon glyphicon-sort-by-attributes-alt"></span></a>
+			            	</li>
+		            	</div>);
+ 			}.bind(this));
+
  		return (
- 			    <div className="pull-right  col-sm-4 col-md-4 col-lg-4 text-right order">
+ 			    <div id={this.props.componentName} className="pull-right  col-sm-4 col-md-4 col-lg-4 text-right order">
 			        Kolejność: 
 			        <div className="btn-group">
 			            <button type="button" className="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">
-			                data publikacji <span className="glyphicon glyphicon-sort-by-attributes-alt"></span> <span className="caret"></span>
+			                {this.state.current.field.label} <span className={icon}></span> <span className="caret"></span>
 			            </button>
 			            <ul className="dropdown-menu" role="menu">
-			                <li><a href="#">data publikacji <span className="glyphicon glyphicon-sort-by-attributes"></span></a></li>
-			                <li><a href="#" className="active">data publikacji <span className="glyphicon glyphicon-sort-by-attributes-alt"></span></a></li>
-			                <li className="divider"></li>
-			                <li><a href="#">nazwa kanału <span className="glyphicon glyphicon-sort-by-attributes"></span></a></li>
-			                <li><a href="#">nazwa kanału <span className="glyphicon glyphicon-sort-by-attributes-alt"></span></a></li>
-			                <li className="divider"></li>
-			                <li><a href="#">tytuł <span className="glyphicon glyphicon-sort-by-attributes"></span></a></li>
-			                <li><a href="#">tytuł <span className="glyphicon glyphicon-sort-by-attributes-alt"></span></a></li>
-			                <li className="divider"></li>
-			                <li><a href="#">najpopularniejsze <span className="glyphicon glyphicon-sort-by-attributes"></span></a></li>
-			                <li><a href="#">najpopularniejsze <span className="glyphicon glyphicon-sort-by-attributes-alt"></span></a></li>
+			                {options}
 			            </ul>
 			        </div>
 			    </div>
